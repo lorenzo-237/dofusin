707062065f48419ab9354877ac40bf96687ebe96
lorenzo-237 <lorenzo.softdev@gmail.com>
2026-07-10 16:26:51 +0200

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
